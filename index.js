88ef9d5b2fb8460fe09f542bda7bafcbbe9abe89
John <connollyjohnf@gmail.com>
2019-03-07 03:00:22 +0000

    assignment completed. Recursive loop will check if it's looking at an array. If it is, it starts the loop again but looking at this nested array

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let total = 0;
+  array.forEach(function(item) {
+    if (Array.isArray(item)) {
+      total += sumItems(item);
+    } else {
+      total += item;
+    }
+  });
+  return total;
 }
-
 module.exports = sumItems;
